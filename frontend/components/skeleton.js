03416ca0d26dc6a204341b9057d4c1dc8b8a69d0
Abenezer <mathewosa9@gmail.com>
2026-08-30 19:25:45 -0600

    feat(components): add customizable skeleton placeholder card generator

---
+++ frontend/components/skeleton.js
@@ -0,0 +1,16 @@
+﻿// Shimmer Skeleton Placeholder Component
+class MerkatoSkeletonComponent {
+    static renderCards(count = 4) {
+        return Array.from({ length: count }).map(() => `
+            <div class="skeleton-card" aria-hidden="true">
+                <div class="skeleton skeleton-img"></div>
+                <div class="skeleton skeleton-tag"></div>
+                <div class="skeleton skeleton-title"></div>
+                <div class="skeleton skeleton-title-short"></div>
+                <div class="skeleton skeleton-price"></div>
+                <div class="skeleton skeleton-btn"></div>
+            </div>
+        `).join('');
+    }
+}
+if (typeof window !== 'undefined') window.MerkatoSkeleton = MerkatoSkeletonComponent;
